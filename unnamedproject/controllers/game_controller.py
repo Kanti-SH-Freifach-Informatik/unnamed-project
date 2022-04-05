c5cf062086bb93d42a94d31b8d07c7c5e8190809
LAPTOP-VSPVT0T4\mario <rodrigo.moralesgonzalez@edu.sh.ch>
2022-04-05 17:22:59 +0200

    card changes on deck of players

--- unnamedproject/controllers/game_controller.py
+++ unnamedproject/controllers/game_controller.py
@@ -5,13 +5,13 @@ from unnamedproject.models.Game import Game
 from unnamedproject.models.GamePlayer import GamePlayer
 from unnamedproject.models.Player import Player
 from unnamedproject.models.Card import Card
-from unnamedproject.utilities.card_utilities import generate_hand_str
+from unnamedproject.utilities.card_utilities import generate_hand_str, is_playable, stringify_hand
 
 # GET /
 def index():
     return 'hello game'
 
-# GET /:game_id
+# GET /:user_id
 def show(game_id):
     return 'hello game number ' + str(game_id)
 
@@ -21,7 +21,6 @@ def create():
     top_card = Card()
     game = Game(active_player= 0, top_card=str(top_card))
     for i, p in enumerate(players):
-        # TODO füge hier Karten zur Hand des Spielers hinzu
         gp = GamePlayer(order=i, hand=generate_hand_str(7))
         gp.player = p
         game.game_players.append(gp)
@@ -29,12 +28,31 @@ def create():
     db.session.commit()
     return render_template("games/show.html", game=game)
 
-# POST /:game_id/:played_card
-def update(game_id, played_card):
+# POST /:user_id
+def update(game_id,played_card):
     game = Game.query.filter_by(id=game_id).first()
-    card = Card(representation=played_card)
+    card = Card (representation = played_card)
+    top_card = Card (representation = game.top_card)
+    if is_playable (top_card,card):
+        player = game.game_players[game.active_player]
+        new_hand = [] 
+        found = False 
+        for c in player.get_hand():
+            if c.value == card.value and c.color == card.color and not found:
+                found = True 
+                continue
+            else:
+                new_hand.append(c)
+        player.hand = stringify_hand(new_hand)      
+        game.top_card = played_card 
+        game.active_player = (game.active_player + 1 ) %4
+        db.session.commit()
+    return render_template("games/show.html", game=game)
 
-# DELETE /:game_id
+
+
+
+# DELETE /:user_id
 def delete(game_id):
     game = Game.query.filter_by(id=game_id).first()
     db.session.delete(game)
