52166789e51b3320070f0722fca1c639f8091216
Sven Kellenberger <sven.kellenberger@kellenberger.tv>
2022-06-14 15:05:12 +0200

    add token to players

--- unnamedproject/models/Player.py
+++ unnamedproject/models/Player.py
@@ -1,8 +1,23 @@
+import random
+import string
+
 from unnamedproject import db
+
+def generate_token():
+    def get_random_string():
+        return ''.join(random.choice(string.ascii_letters) for i in range(30))
+
+    token = get_random_string()
+    while Player.query.filter_by(token=token).limit(1).first() is not None:
+        token = get_random_string()
+
+    return token
+
 
 class Player(db.Model):
     __tablename__ = 'players'
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    token = db.Column(db.String(30), default=generate_token)
     games = db.relationship('GamePlayer', back_populates='player')
