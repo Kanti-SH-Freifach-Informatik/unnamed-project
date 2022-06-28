bcd46f80ed2fc75491cc93ea08746fddb0f3b2c3
Sven Kellenberger <sven.kellenberger@kellenberger.tv>
2022-06-28 17:06:27 +0200

    fix message model

--- unnamedproject/models/Message.py
+++ unnamedproject/models/Message.py
@@ -6,6 +6,8 @@ from unnamedproject.models.Game import Game
 class Message(db.Model):
     _tablename__ = 'message'
     id = db.Column(db.Integer, primary_key=True)
+    game_id = db.Column(db.ForeignKey('games.id'))
+    player_id = db.Column(db.ForeignKey('players.id'))
     message = db.Column(db.Text, nullable=False)
     datetime = db.Column(db.DateTime, nullable=False, default = lambda: datetime.now())
     player = db.relationship("Player")
